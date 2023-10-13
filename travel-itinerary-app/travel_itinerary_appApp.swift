581e85895118dde253a292ab53d6bded3003d8c9
yanoojp <120234755+yanoojp@users.noreply.github.com>
2023-10-13 10:29:21 +0900

    Initial commit

---
+++ travel-itinerary-app/travel_itinerary_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  travel_itinerary_appApp.swift
+//  travel-itinerary-app
+//
+//  Created by ysnoojp on 2023/10/13.
+//
+
+import SwiftUI
+
+@main
+struct travel_itinerary_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ travel-itinerary-appUITests/travel_itinerary_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  travel_itinerary_appUITestsLaunchTests.swift
+//  travel-itinerary-appUITests
+//
+//  Created by ysnoojp on 2023/10/13.
+//
+
+import XCTest
+
+final class travel_itinerary_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
